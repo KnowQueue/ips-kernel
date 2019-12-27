da6414c3043c32e3a4010d1864d657fe29c9440c
Phuoc Do <binanhphuoc@gmail.com>
2019-12-26 19:04:17 -0600

    Able to create class dynamically using names and strings

--- knowledgebase/concepts/test.py
+++ knowledgebase/concepts/test.py
@@ -1,21 +1,34 @@
 
-def constructor(self, *prototype):
-    instance_key = self.__class__.__name__
+instances = {}
+
+def constructor(cls, *prototype):
+    instance_key = cls.__name__
     for letter in prototype:
         instance_key += "_{:s}".format(letter)
-    if instance_key in self.__class__.instances:
-        return self.__class__.instances[instance_key]
+    if instance_key in instances:
+        return instances[instance_key]
     else:
-        self.__class__.instances[instance_key] = self
+        instances[instance_key] = getattr(cls, cls.__name__[:(len(cls.__name__)-len("_Mixin"))])()
+        return instances[instance_key]
 
-globals()["TRIANGLE"] = type("TRIANGLE",(object,),{
-    "instances": {},
-    "__init__": constructor,
+def initializer(self):
+    print("Created a new object")
+
+globals()["TRIANGLE"] = type("TRIANGLE_Mixin",(object,),{
+    "TRIANGLE": type("TRIANGLE",(object,), {
+        "__init__": initializer
+    }),
+    "__new__": constructor,
 })
 
-
+string = """
 a = TRIANGLE("A","B","C")
 print(a)
-
 b = TRIANGLE("A","B","C")
 print(b)
+c = TRIANGLE("A","B","F")
+print(c)
+"""
+
+exec(string)
+
