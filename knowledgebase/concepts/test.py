1f59b130fb68e2a62bfd46f19fa682ac9ebf4947
Phuoc Do <binanhphuoc@gmail.com>
2019-12-26 23:08:29 -0600

    memory per request

--- knowledgebase/concepts/test.py
+++ knowledgebase/concepts/test.py
@@ -1,36 +1,41 @@
 
-instances = {}
-
 def constructor(cls, *prototype):
+    working_mem = cls.working_mem
     instance_key = cls.__name__
     for letter in prototype:
         instance_key += "_{:s}".format(letter)
-    if instance_key in instances:
-        return instances[instance_key]
+    if instance_key in working_mem["instances"]:
+        return working_mem["instances"][instance_key]
     else:
         inner_class_name = cls.__name__[:(len(cls.__name__)-len("_Mixin"))]
         inner_class = getattr(cls, inner_class_name)
-        instances[instance_key] = inner_class()
-        return instances[instance_key]
+        working_mem["instances"][instance_key] = inner_class(prototype)
+        return working_mem["instances"][instance_key]
 
-def initializer(self):
-    print("Created a new object")
-
-globals()["TRIANGLE"] = type("TRIANGLE_Mixin",(object,),{
-    "TRIANGLE": type("TRIANGLE",(object,), {
-        "__init__": initializer
-    }),
-    "__new__": constructor,
-})
+def initializer(self, prototype):
+    print(prototype)
 
 string = """
 a = TRIANGLE("A","B","C")
-print(a)
 b = TRIANGLE("A","B","C")
-print(b)
 c = TRIANGLE("A","B","F")
-print(c)
 """
 
-exec(string)
+def session(string):
+    working_mem = {
+        "instances": {}
+    }
+
+    locals()["TRIANGLE"] = type("TRIANGLE_Mixin",(object,),{
+        "TRIANGLE": type("TRIANGLE",(object,), {
+            "__init__": initializer
+        }),
+        "working_mem": working_mem,
+        "__new__": constructor,
+    })
+
+    exec(string, globals(), locals())
+
+session(string)
+session(string)
 
