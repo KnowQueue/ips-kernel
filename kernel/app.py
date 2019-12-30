f4fb1823101614a63f3faec90a42d3e736a951fe
Phuoc Do <binanhphuoc@gmail.com>
2019-12-30 13:33:50 -0500

    Done deploying

--- kernel/app.py
+++ kernel/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask import request
 from .parsers.global_loader import global_loader
 from .parsers.request_session import request_session
+from settings import APP_KB
+import os
+import shutil
+from pathlib import Path
 
 app = Flask(__name__)
 kernel_globals = {
@@ -20,9 +24,16 @@ def query():
     # exec(request.get_data())
     # return result
 
-@app.route("/deploy")
+@app.route("/deploy", methods=['PUT'])
 def deploy():
-    return "Hello, World!"
+    shutil.rmtree(APP_KB)
+    for filePath in request.files.keys():
+        directoryPath = os.path.dirname(os.path.join(APP_KB, filePath))
+        Path(directoryPath).mkdir(parents=True, exist_ok=True)
+        Path(os.path.join(APP_KB, filePath)).touch()
+        request.files[filePath].save(os.path.join(APP_KB, filePath))
+        request.files[filePath].close()
+    return str([key for key in request.files.keys()])
 
 @app.route("/clone")
 def clone():
